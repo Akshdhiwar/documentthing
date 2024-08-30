ef438e4d67f918c88f25eb009e0bd15a8e813a13
akash <akashdhiwar3001@gmail.com>
2024-08-30 21:11:34 +0530

    added : back button for setting

--- src/pages/Docs/ProjectSettings.tsx
+++ src/pages/Docs/ProjectSettings.tsx
@@ -1,17 +1,22 @@
 import Sidenav from "@/components/main/DocsPage/Settings/Sidenav"
 import { Separator } from "@/components/ui/separator"
-import { Outlet } from "react-router-dom"
+import { ChevronLeft } from "lucide-react"
+import { Link, Outlet } from "react-router-dom"
 
 
 const ProjectSettings = () => {
     return (
         <>
             <div className="hidden space-y-6 h-full p-10 pb-16 md:flex flex-col">
-                <div className="space-y-0.5">
-                    <h2 className="text-2xl font-bold tracking-tight">Settings</h2>
-                    <p className="text-muted-foreground">
-                        Manage your account settings and set e-mail preferences.
-                    </p>
+                <div className="flex items-center gap-8">
+                    <Link to={".."}><ChevronLeft></ChevronLeft>
+                    </Link>
+                    <div className="space-y-0.5">
+                        <h2 className="text-2xl font-bold tracking-tight">Settings</h2>
+                        <p className="text-muted-foreground">
+                            Manage your account settings and set e-mail preferences.
+                        </p>
+                    </div>
                 </div>
                 <Separator className="my-6" />
                 <div className="flex flex-col flex-1 space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
