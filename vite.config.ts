39adfe5444796d7d43594ae96aa9616dc2bd2790
akash <akashdhiwar3001@gmail.com>
2024-11-19 00:56:39 +0530

    updates fixed

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,10 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import favicons from "@peterek/vite-plugin-favicons";
 
-const imagePath = path.resolve(__dirname, "src/assets/Documentthing.png");
-console.log("Resolved image path:", imagePath); // Log the resolved path
+const imagePath = path.resolve(__dirname, "./src/assets/Documentthing.png");
 
 export default defineConfig({
-  plugins: [
-    react(),
-    favicons(imagePath),
-  ],
+  plugins: [react(), favicons(imagePath)],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
