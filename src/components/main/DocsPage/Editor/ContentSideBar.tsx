c65fda74e90304a56f443811ccd1deecb34f4dd4
akash <akashdhiwar3001@gmail.com>
2024-12-31 14:58:07 +0530

    updates done

--- src/components/main/DocsPage/Editor/ContentSideBar.tsx
+++ src/components/main/DocsPage/Editor/ContentSideBar.tsx
@@ -4,16 +4,19 @@ const ContentSideBar = () => {
     const { sidebarContents } = useEditorStore((state) => state);
 
     return (
-        <div className="p-3 pt-0 text-muted-foreground font-light">
-            {sidebarContents.reverse().map((content: any) => (
-                <div className="flex w-full" key={content.id}>
-                    <a
-                        href={`#${content?.value?.[0]?.id}`}
-                        className="flex flex-1 basis-0 truncate hover:text-secondary-foreground hover:cursor-pointer hover:underline"
-                    >
-                        {content?.value?.[0]?.children?.[0]?.text || "Untitled"}
-                    </a>
-                </div>
+        <div className="p-3 pt-0 text-sm">
+            <p className="font-medium">On This Page</p>
+            {sidebarContents.map((content: any) => (
+                <ul className="flex w-full m-0 list-none" key={content.id}>
+                    <li className="mt-0 pt-2">
+                        <a
+                            href={`#${content?.value?.[0]?.id}`}
+                            className="inline-block no-underline transition-colors hover:text-foreground text-muted-foreground"
+                        >
+                            {content?.value?.[0]?.children?.[0]?.text || "Untitled"}
+                        </a>
+                    </li>
+                </ul>
             ))}
         </div>
     );
